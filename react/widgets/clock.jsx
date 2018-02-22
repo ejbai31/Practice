899a64da5f57bac4491f671248a049c83b3b9c34
Edward Bai <edwardbai31@gmail.com>
2018-02-21 21:38:04 -0800

    Add DOM hook

--- react/widgets/clock.jsx
+++ react/widgets/clock.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 class Clock extends React.Component{
-  
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Clock</h1>
+      </div>
+    );
+  }
 }
 
 export default Clock;

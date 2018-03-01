abc490c14adf5c95ede355093d900c00adaec867
Edward Bai <edwardbai31@gmail.com>
2018-02-28 19:41:57 -0800

    Fill out weather

--- react/widgets/widgets.jsx
+++ react/widgets/widgets.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
 
-document.addEventListener("DOMContentLoaded", (event) => {
-	const root = document.getElementById("root");
+document.addEventListener("DOMContentLoaded", () => {
+	const root = document.getElementById('root');
 	ReactDOM.render(<Root />, root);
 });

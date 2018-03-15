1e2af5f163ed1376f3fa15f57248fec1909fcaab
Edward Bai <edwardbai31@gmail.com>
2018-03-14 20:48:59 -0700

    Initial tabs commit

---
+++ react/widgets/frontend/tabs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Headers extends React.Component {
+  render(){
+    let selected = this.props.selectedPane;
+    let headers = this.props.panes.map((pane, index) => {
+      let title = pane.title;
+      let klass = '';
+      if(index === selected) {
+        klass = 'active';
+      }
+      return (
+        <li
+          key = {index}
+          className = {klass}
+          onClick = {this.props.onTabChosen.bind(null, index)}
+        >
+        {title}
+        </li>
+      );
+    });
+  }
+}
